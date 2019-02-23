0caece308f9b51d1419ab6d66c99aa17c7b62bac
Matthew Healy <mhrh3@mst.edu>
2019-02-22 21:48:50 -0600

    Remember that I am writing Python

--- color_triangles.py
+++ color_triangles.py
@@ -37,7 +37,7 @@ while true:
             tri_coords.append([cnt])
         elif len(approx) == 4:
             sq_coords.append([cnt])
-        elif len(approx) > 8 && cv2.contourArea(cnt): # lots of verticies but not miniscule
+        elif len(approx) > 8 and cv2.contourArea(cnt): # lots of verticies but not miniscule
             cc_coords.append([cnt])
             
     # color contors
